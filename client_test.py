81ada4e9e2502159a2a1a5a19db4fa8f8a2e9948
AA Student <appacademy@Mini-113.local>
2023-07-07 13:38:27 -0700

    Test getRatio and getDataPiont

--- client_test.py
+++ client_test.py
@@ -1,5 +1,6 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
+
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +9,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +18,26 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio_price_a_zero(self):
+    prices = [
+      {'price_a': 0, 'price_b': 119.2},
+      {'price_a': 0, 'price_b': 121.68}
+    ]
+    for price in prices:
+      self.assertEqual(getRatio(price['price_a'], price['price_b']), (price['price_a']/price['price_b']))
 
+  def test_getRatio_price_b_zero(self):
+    prices = [
+      {'price_a': 119.2, 'price_b': 0},
+      {'price_a': 121.68, 'price_b': 0}
+    ]
+    for price in prices:
+      self.assertEqual(getRatio(price['price_a'], price['price_b']), (price['price_a']/price['price_b']))
 
 if __name__ == '__main__':
     unittest.main()
